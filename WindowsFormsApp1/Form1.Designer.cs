a12cd838683fd162d0d72339ba1640e212a8e2bd
AbyssRat <yunothefurry@gmail.com>
2026-01-30 13:58:28 +0100

    szép lett :p

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 450);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(153, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(250, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Read";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(250, 75);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +103,12 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -22,12 +22,101 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
-            MessageBox.Show("API URL: " + ApiUrl); // Display the API URL for verification
+            MessageBox.Show("szerintem szép :p \nAPI URL: " + ApiUrl); // Display the API URL for verification
         }
 
 
         private void Form1_Load(object sender, EventArgs e)
         {
+           
+
+  
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            //display all names in listbox from Adat class
+
+
+        
+
+
+
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //Create button POST request to ApiUrl
+            var values = new Dictionary<string, string>
+            {
+                { "Name", "New Name" },
+                { "cvv", "123" },
+                { "Alive", "true" }
+            };
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync(ApiUrl, content);
+            var responseString = await response.Content.ReadAsStringAsync();
+            MessageBox.Show("Response: " + responseString);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Create button UPDATE request to ApiUrl
+            if (listBox1.SelectedItem is Adat selectedItem)
+            {
+                var values = new Dictionary<string, string>
+                {
+                    { "id", selectedItem.Id.ToString() },
+                    { "Name", "Updated Name" },
+                    { "cvv", "456" },
+                    { "Alive", "false" }
+                };
+                var content = new FormUrlEncodedContent(values);
+                var response = client.PutAsync(ApiUrl + "/" + selectedItem.Id, content).Result;
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                MessageBox.Show("Response: " + responseString);
+            }
+            else
+            {
+                MessageBox.Show("Please select an item to update.");
+            }
+
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Create button READ request to ApiUrl
+            var response = client.GetAsync(ApiUrl).Result;
+            var responseString = response.Content.ReadAsStringAsync().Result;
+            var adatArray = Adat.FromJson(responseString);
+            items = new BindingList<Adat>(adatArray.ToList());
+            listBox1.DataSource = items;
+            listBox1.DisplayMember = "Name";
+            listBox1.SelectedIndex = 0;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Create button DELETE request to ApiUrl
+            if (listBox1.SelectedItem is Adat selectedItem)
+            {
+                var response = client.DeleteAsync(ApiUrl + "/" + selectedItem.Id).Result;
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                MessageBox.Show("Response: " + responseString);
+            }
+            else
+            {
+                MessageBox.Show("Please select an item to delete.");
+            }
+
         }
     }
 }
